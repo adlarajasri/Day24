e10623d4be57e3b52856ddb7efbba324ce20c774
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-05-08 09:35:48 +0530

    Add files via upload

---
+++ EntityFrameworkDemo/EntityFrameworkDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Assignment2Entities())
+            {
+                var std = new Client_Master()
+                {
+                    ClientNo = "C00002",
+
+                    Name = "Bill",
+
+                    Address1 = "123 street",
+
+                    Address2 = "abc street",
+
+                    City = "Kochi",
+
+                    State = "Kerala",
+
+                    BalDue = 40000
+
+                    
+
+
+                };
+                context.Client_Master.Add(std);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
