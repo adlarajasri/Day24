a28beeb8c76587e7904317949b27cb8a2c1a6b3f
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-05-08 09:32:39 +0530

    Add files via upload

---
+++ EfCodeFirst/EfCodeFirst/Entities/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCodeFirst.Entities
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        public string StudentName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public byte[] Photo { get; set; }
+
+        public decimal Height { get; set; }
+
+        public float Weight { get; set; }
+
+        public Grade Grade { get; set; }
+
+    }
+}
+
+    
+
