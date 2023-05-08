40216b1c023cbf116ca09e8fa4b32c05e1cc60a0
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-05-08 09:34:16 +0530

    Add files via upload

---
+++ EmpModel2/EmpModel2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpModel2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpDBEntities e=new EmpDBEntities();
+            foreach(var item in e.Employees) {
+                Console.WriteLine(item.EmpName);
+            }
+        }
+    }
+}
